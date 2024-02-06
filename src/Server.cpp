d60dc251f56a28d5dac2bb60b960c876d21f60be
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2024-02-06 11:59:38 -0800

    Forth stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <unordered_set>
 
+bool match_positive_char(const std::string& inputs, const std::string& pattern) {
+    std::unordered_set<char> pattern_set(pattern.begin(), pattern.end());
+    for (auto input : inputs) {
+        if (pattern_set.find(input) != pattern_set.end()) {
+            return true;
+        }
+    }
+    return false;
+}
 bool match_alphanumeric(const std::string& inputs, const std::string& pattern) {
    for(auto input : inputs) {
         if (isalpha(input) || isdigit(input) || input == '_') {
@@ -29,6 +39,13 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
     }
     else if (pattern == "\\w") {
         return match_alphanumeric(input_line, pattern);
+    }
+    else if (pattern.front() == '[' && pattern.back() == ']') {
+        std::string trim_pattern = "";
+        if (pattern.size() > 2) {
+            trim_pattern = pattern.substr(1, pattern.size() - 2);
+        }
+        return match_positive_char(input_line, trim_pattern);
     }
     else {
         throw std::runtime_error("Unhandled pattern " + pattern);
