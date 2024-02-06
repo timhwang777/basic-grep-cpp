da4926aec6f65277c7fe6d3dd389c9579d51fe9d
Tim@Davis-Dorm-WSL2 <timhwang@ucdavis.edu>
2024-02-06 10:49:02 -0800

    First stage

--- src/Server.cpp
+++ src/Server.cpp
@@ -11,9 +11,6 @@ bool match_pattern(const std::string& input_line, const std::string& pattern) {
 }
 
 int main(int argc, char* argv[]) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    std::cout << "Logs from your program will appear here" << std::endl;
-
     if (argc != 3) {
         std::cerr << "Expected two arguments" << std::endl;
         return 1;
@@ -27,19 +24,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    // Uncomment this block to pass the first stage
-    //
-    // std::string input_line;
-    // std::getline(std::cin, input_line);
-    //
-    // try {
-    //     if (match_pattern(input_line, pattern)) {
-    //         return 0;
-    //     } else {
-    //         return 1;
-    //     }
-    // } catch (const std::runtime_error& e) {
-    //     std::cerr << e.what() << std::endl;
-    //     return 1;
-    // }
+    std::string input_line;
+    std::getline(std::cin, input_line);
+
+    try {
+        if (match_pattern(input_line, pattern)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } catch (const std::runtime_error& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
